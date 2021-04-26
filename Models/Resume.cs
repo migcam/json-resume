67122d80fbf5e811a2c15b49ec030d6fb54ad940
migcam <41090100+migcam@users.noreply.github.com>
2021-04-25 21:31:15 -0400

    seed data

--- Models/Resume.cs
+++ Models/Resume.cs
@@ -40,6 +40,79 @@ namespace json_resume.Models
                 }
             };
 
+            works = new List<Work>();
+            works.Add(new Work{
+                company = "mediatrix",
+                position= "developer",
+                website = "https://mediatrix.com.do",
+                startDate = new DateTime(2020,11,2),
+                summary = "contratista del banco BHD",
+            });
+
+            volunteers = new List<Volunteer>();
+            volunteers.Add(new Volunteer{
+                organization = "scouts dominicanos",
+                position ="subguia",
+                website = "https://scouts.do",
+                startDate = new DateTime(2011,1,1),
+                endDate = new DateTime(2019,9,1)
+            });
+
+            educations = new List<Education>();
+            educations.Add(new Education{
+                institution = "INTEC",
+                area = "Software",
+                studyType = "Bachelor",
+                startDate = new DateTime(2017,11,2),
+                endDate = new DateTime(2021,6,28),
+                gpa = 3.77
+            });
+
+            awards = new List<Award>();
+            awards.Add(new Award{
+                title = "erasmus",
+                date = new DateTime(2019,9,23),
+                awarder = "Union Europea",
+                summary = "Intercambio estudiantil"
+            });
+
+            skills = new List<Skill>();
+            skills.Add(new Skill{
+                name = "programming",
+                level = "mid",
+                keywords = new List<string>{
+                    "ASP.NET core",
+                    "Algorithms",
+                    "Typescript"
+                }
+            });
+
+            languages = new List<Language>();
+            languages.Add(new Language{
+                language  = "spanish",
+                fluency = "native"
+            });
+
+
+            interests = new List<Interest>();
+            interests.Add(new Interest{
+                name = "wildlife",
+                keywords = new List<string>{
+                    "traveling",
+                    "campming",
+                    "sports"
+                }
+            });
+
+
+            references = new List<Reference>();
+            references.Add(new Reference{
+                name = "francia mejia",
+                reference = "fmejia@intec.edu.do"
+            });
+
+
+
         }
 
     }
